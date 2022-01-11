4df6cdf240e11124826e9139597e0bae9d25ac0a
llamar <thatlivelyllama@gmail.com>
2022-01-11 14:22:53 +0000

    Fetching deadpool data from marvel api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import md5 from "md5"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      value: "",
+    }
+  }
+
+  onSubmit = event => {
+    event.preventDefault();
+    const {value} = this.state
+    const baseURL = `https://gateway.marvel.com:443/v1/public/`
+    const searchType = `characters?`
+    const searchMethod = `name=`
+    const timestamp = Date.now()
+    const publicKey = process.env.REACT_APP_PUBLIC_KEY
+    const privateKey = process.env.REACT_APP_PRIVATE_KEY
+    const hash = md5(timestamp + privateKey + publicKey)
+    fetch(`${baseURL}${searchType}${searchMethod}${value}${timestamp}${publicKey}${hash}`)
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }
+
+  render() {
+    const timestamp = Date.now()
+    const publicKey = process.env.REACT_APP_PUBLIC_KEY
+    const privateKey = process.env.REACT_APP_PRIVATE_KEY
+    const hash = md5(timestamp + privateKey + publicKey)
+    // console.log("hash", hash)
+    fetch(`https://gateway.marvel.com:443/v1/public/characters?name=Deadpool&ts=${timestamp}&apikey=${publicKey}&hash=${hash}`)
+      .then(response => response.json())
+      .then(data => {
+        if (data.code === 200) {
+          console.log(data)
+        }
+      })
+    return (
+      <div className="App">
+        {timestamp}
+      </div>
+    );
+  }
 }
 
 export default App;
