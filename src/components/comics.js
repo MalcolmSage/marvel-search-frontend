28ef0ab4e9dd90b241906bda7d2ecc6acb2f975c
llamar <thatlivelyllama@gmail.com>
2022-01-16 12:21:28 +0000

    removed unused imports

--- src/components/comics.js
+++ src/components/comics.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 
-import { Grid, Typography } from "@mui/material/";
+import { Grid } from "@mui/material/";
 import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import { CardActionArea } from '@mui/material';
 
